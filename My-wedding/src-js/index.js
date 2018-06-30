0271ee29b94eec7eed4965fbd6fe9529ecbe0ed3
KidyasoVasiliy <vasiliy.kid@gmail.com>
2018-06-30 19:32:16 +0300

    scroll v1

--- My-wedding/src-js/index.js
+++ My-wedding/src-js/index.js
@@ -3,9 +3,11 @@
 	import getIntoTheAccount from './parts/getIntoTheAccount.js';
 	import goToCustomizationPage from './parts/goToCustomizationPage.js';
 	import windowWithTariff from './parts/windowWithTariff.js';
+	import scroll from './parts/scroll.js';
 
 
 	mainSlider();
 	getIntoTheAccount();
 	goToCustomizationPage();
 	windowWithTariff();
+	scroll();
