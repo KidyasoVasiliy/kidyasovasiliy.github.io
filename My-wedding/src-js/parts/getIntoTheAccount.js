f76c1f00cb7e13fe89cff3b1e71573f479f12573
KidyasoVasiliy <vasiliy.kid@gmail.com>
2018-06-30 16:11:19 +0300

    Модальное окно входа v2

--- My-wedding/src-js/parts/getIntoTheAccount.js
+++ My-wedding/src-js/parts/getIntoTheAccount.js
@@ -2,19 +2,33 @@ function getIntoTheAccount(){
 	let headerGetInThAcBtn = document.querySelector('.header-login__button'),
 		footerGetInThAcBtn = document.querySelector('.footer-login__button'),
 		overlay = document.querySelector('.overlay'),
-		closeBtnForm = document.querySelector('.popup-close');
+		closeBtnForm = document.querySelector('.popup-close'),
+		popupForm = document.querySelector('.popup');
 
-	headerGetInThAcBtn.addEventListener('click', ()=>{
+	headerGetInThAcBtn.addEventListener('click', ()=>{ // Открываем окно, если нажали "Войти"
 		overlay.style.display = 'block';
 		document.body.style.overflow = 'hidden'
+		eventOutside()
 	})	
-	footerGetInThAcBtn.addEventListener('click', ()=>{
+	footerGetInThAcBtn.addEventListener('click', ()=>{ // Открываем окно, если нажали "Войти"
 		overlay.style.display = 'block';
 		document.body.style.overflow = 'hidden'
+		eventOutside()
 	})
-	closeBtnForm.onclick = function(){
+	closeBtnForm.onclick = function(){ // Закрываем модальное окно, если клик на крестик
 		overlay.style.display = 'none';
 		document.body.style.overflow = '';
+	}
+
+	function eventOutside() {   //Закрываем модальное окно, если клик вне формы
+		overlay.onclick = (e) => {
+			console.log('Тык')
+			if (e.path.length===5) {
+				overlay.style.display = 'none';
+				document.body.style.overflow = '';
+				console.log('Опа, закрыли модалку)')
+			}
+		}	
 	}
 
 	//AJAX
